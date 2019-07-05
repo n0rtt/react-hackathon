4d296fcd67d47f9903fb3f3c11e8189d32dd9eb3
Miguel Osorio <mial.osorio@gmail.com>
2019-07-05 11:41:34 +0200

    spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <style className="lds-css ng-scope"><div className="lds-pacman"><div><div></div><div></div><div></div></div><div><div></div><div></div></div></div><style type="text/css">
+        </style></style>
+    );
+}
+
+export default Spinner;
+
