838257e3a6fa39df07318b3caedbaac9f569b4df
ErikMille <miller.erik.1998@gmail>
2019-07-05 11:38:43 +0200

    search final

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,9 +7,19 @@ const App = () => {
     const [flights, setFlights] = useState([])
     const [currency, setCurrency] = useState([])
     const [loading, setLoading] = useState(false)
+    const [city, setCity] = useState('VLC')
+
+    const handleChange=(e)=>{
+        setCity(e.target.value)
+        console.log(city)
+    }
+    const handleClick=()=>{
+        setLoading(true)
+        updateFlights()
+    }
 
     const updateFlights = () => {
-        fetch('https://api.skypicker.com/flights?flyFrom=PRG&to=VLC&dateFrom=06/07/2019&partner=picky&limit=5')
+        fetch(`https://api.skypicker.com/flights?flyFrom=PRG&to=${city}&dateFrom=06/07/2019&partner=picky&limit=5`)
             .then(res => res.json())
             .then(data => {
                 setFlights(data.data)
@@ -18,14 +28,12 @@ const App = () => {
             })
     }
 
-    useEffect(() => {
-        setLoading(true)
-        updateFlights()
-    }, [])
+   
 
+    
     return (
         <>
-            <Search/>
+            <Search handleChange={handleChange} handleClick={handleClick}/>
             {(loading) && <div>Loading...</div>}
             <FlightList flights={flights} currency={currency} />
 
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Search=(handleChange)=>(
+const Search=({handleChange,handleClick})=>(
         <div>
             <select name="" id="" onChange={handleChange}>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                <option value="VLC">Valencia</option>
+                <option value="BCN">Barcelona</option>
+                <option value="MAD">Madrid</option>
+                <option value="MXP">Milano</option>
+                <option value="ATH">Athens</option>
             </select>
+            <button onClick={handleClick}>Search</button>
         </div>
 )
 
