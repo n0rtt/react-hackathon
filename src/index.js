0da4b9296d470d4b6fc7202824595ce5a3f0791e
Miguel Osorio <mial.osorio@gmail.com>
2019-07-05 09:33:01 +0200

    structure

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Message from './message/message.jsx';
+import App from './components/app/App'
 import './index.scss';
 import './index.html';
 
-class Hello extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Hello world</h1>
-        <Message title="My title" body="hello body" />
-        <Message title="My second title" body="body body body" />
-        <Message title="Last title" body="show me your body" />
-      </>
-    );
-  }
-}
+ReactDOM.render(<App />, document.querySelector('#app'));
 
-ReactDOM.render(<Hello />, document.querySelector('#app'));
