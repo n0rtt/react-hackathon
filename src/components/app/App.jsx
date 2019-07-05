b202f560b5c69583a8cacf6a4fba757830ccf69c
Miguel Osorio <mial.osorio@gmail.com>
2019-07-05 10:13:33 +0200

    fetch link

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+
+    const [flights, setFlights] = useState([])
+
+    const updateFlights = () => {
+        fetch('https://api.skypicker.com/flights?flyFrom=PRG&to=VLC&dateFrom=06/07/2019&partner=picky&limit=5')
+            .then(res => res.json())
+            .then(data => console.log(data))
+
+    }
+
+    useEffect(() => {
+        updateFlights()
+    })
+
     return (
         <>
+            {}
         </>
     );
 }
