9d4fb4ba2db5dce0d902487ba3c7fdaffaa973d1
Miguel Osorio <mial.osorio@gmail.com>
2019-07-05 14:54:36 +0200

    scss

--- src/components/flight/Flight.jsx
+++ src/components/flight/Flight.jsx
@@ -2,26 +2,25 @@ import React from 'react'
 import { DateTime } from 'luxon';
 
 const Flight = ({ flight, currency }) => (
-    <div>
-        <div className='flight'>
-            <div className='cities'>
-                Departure: <div>{flight.cityFrom}</div>
-                <div>{flight.flyFrom}</div>
-            </div>
-            <div className='cities'>
-                Arrival: <div>{flight.cityTo}</div>
-                <div>{flight.flyTo}</div>
-            </div>
-            <div>
-                <div>{DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm')}</div>
-                <div>{DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm')}</div>
-            </div>
-            <div>
-                <div>{flight.price}{currency}</div>
-            </div>
-            <div>
-                <div>{flight.route.length - 1}</div>
-            </div>
+
+    <div className='flight'>
+        <div className='cities'>
+            <div>{flight.cityFrom}</div>
+            <div>{flight.flyFrom}</div>
+        </div>
+        <div className='cities'>
+            <div>{flight.cityTo}</div>
+            <div>{flight.flyTo}</div>
+        </div>
+        <div className='time'>
+            <div>Departure time: {DateTime.fromMillis(flight.dTime * 1000).toFormat('hh:mm')}</div>
+            <div>Arrival time: {DateTime.fromMillis(flight.aTime * 1000).toFormat('hh:mm')}</div>
+        </div>
+        <div className='price'>
+            <div>Price: {flight.price}{currency}</div>
+        </div>
+        <div className='stopovers'>
+            <div>Number of stopovers: {flight.route.length - 1}</div>
         </div>
     </div>
 )
