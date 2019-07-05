380ed5f868126151313d84cb212d8a6810b6a611
Miguel Osorio <mial.osorio@gmail.com>
2019-07-05 14:09:04 +0200

    layout

--- src/components/flight/Flight.jsx
+++ src/components/flight/Flight.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
 
 const Flight = ({ flight, currency }) => {
     return (
-        <div>
-            <div>{flight.cityFrom}</div>
-            <div>{flight.flyFrom}</div>
-            <div>{flight.cityTo}</div>
-            <div>{flight.flyTo}</div>
-            <div>{flight.dTime}</div>
-            <div>{flight.aTime}</div>
-            <div>{flight.price}{currency}</div>
-            <div>{flight.route.length - 1}</div>
-        </div>
+        <Container>
+            <Col>
+                <Row>
+                    <div>{flight.cityFrom}</div>
+                    <div>{flight.flyFrom}</div>
+                </Row>
+                <Row>
+                    <div>{flight.cityTo}</div>
+                    <div>{flight.flyTo}</div>
+                </Row>
+                <Row>
+                    <div>{flight.dTime}</div>
+                    <div>{flight.aTime}</div>
+                </Row>
+                <Row>
+                    <div>{flight.price}{currency}</div>
+                </Row>
+                <Row>
+                    <div>{flight.route.length - 1}</div>
+                </Row>
+            </Col>
+        </Container>
     )
 }
 
