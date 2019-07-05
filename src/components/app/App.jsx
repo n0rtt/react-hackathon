b9e54d6c8266e75776dc0393f4e6df8dc40c9625
ErikMille <miller.erik.1998@gmail>
2019-07-05 12:41:51 +0200

    aaaa

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -8,11 +8,21 @@ const App = () => {
     const [flights, setFlights] = useState([])
     const [currency, setCurrency] = useState([])
     const [loading, setLoading] = useState(false)
-    const [city, setCity] = useState('VLC')
+    const [city, setCity] = useState({departure:'PRG',arrival:'VLC'})
 
-    const handleChange = (e) => {
+    const handleChangeDeparture = (e) => {
         setCity(e.target.value)
-        console.log(city)
+        console.log(city.departure)
+    }
+    const handleChangeArrival = (e) => {
+        setCity((prevState)=>{
+            return {
+                ...prevState,
+                arrival:e.target.value,
+            }
+        }
+        )
+        console.log(city.arrival)
     }
     const handleClick = () => {
         setLoading(true)
@@ -20,7 +30,7 @@ const App = () => {
     }
 
     const updateFlights = () => {
-        fetch(`https://api.skypicker.com/flights?flyFrom=PRG&to=${city}&dateFrom=06/07/2019&partner=picky&limit=5`)
+        fetch(`https://api.skypicker.com/flights?flyFrom=PRG&to=${city.departure}&dateFrom=06/07/2019&partner=picky&limit=5`)
             .then(res => res.json())
             .then(data => {
                 setFlights(data.data)
@@ -32,7 +42,7 @@ const App = () => {
     return (
         <>
 
-            <Search handleChange={handleChange} handleClick={handleClick} />
+            <Search handleChangeDeparture={handleChangeDeparture} handleChangeArrival={handleChangeArrival} handleClick={handleClick} />
             {(loading) && <Spinner />}
             <FlightList flights={flights} currency={currency} />
 
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Search=({handleChange,handleClick})=>(
+const Search=({handleChangeArrival,handleChangeDeparture,handleClick})=>(
         <div>
-            <select name="" id="" onChange={handleChange}>
+            <select name="" id="" onChange={handleChangeDeparture}>
+                <option value="PRG">Prague</option>
+                <option value="BER">Berlin</option>
+                <option value="WAW">Warsaw</option>
+                <option value="PED">Pardubice</option>
+            </select>
+            <select name="" id="" onChange={handleChangeArrival}>
                 <option value="VLC">Valencia</option>
                 <option value="BCN">Barcelona</option>
                 <option value="MAD">Madrid</option>
